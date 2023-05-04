805587d16c87f06a32019ab9db5e534928d9fa00
aureldpz <aureldpz@Xubuntu.myguest.virtualbox.org>
2023-05-04 20:13:32 +0200

    exo 2.1.4

--- db/migrate/20230504155718_link_appointments.rb
+++ db/migrate/20230504155718_link_appointments.rb
@@ -1,6 +1,6 @@
 class LinkAppointments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :appointments, :doctor, null: false, foreign_key: true
+    
     add_reference :appointments, :patient, null: false, foreign_key: true
-  end
+  endadd_reference :appointments, :doctor, null: false, foreign_key: true
 end
